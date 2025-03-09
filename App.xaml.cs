aa04c324512f626567a23e4cfe2c764516cdf3e0
qwerro <serbushko.qwe@gmail.com>
2025-03-09 14:36:40 +0700

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace appForAccGoods
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace appForAccGoods
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
